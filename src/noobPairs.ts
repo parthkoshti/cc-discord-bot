ac68b9856de1138e3fbcf84dde8f876f38845d5b
parthkoshti <30394245+parthkoshti@users.noreply.github.com>
2025-01-24 22:00:46 +0530

    noob message fixed

--- src/noobPairs.ts
+++ src/noobPairs.ts
@@ -2,7 +2,7 @@ export const noobPairs = [
   {
     listenFor: ["new"],
     responses: [
-      "Welcome to the server, newbie. Go to https://discord.com/channels/627586215905853451/922612850457997323/922873959291170856, <#922612850457997323> and <#922573595115421796> to get started! Do that first and then drop any other questions you have in <#627907039808585758>",
+      "Welcome to the server, newbie. Go to https://discord.com/channels/627586215905853451/922612850457997323/922873959291170856 and <#922573595115421796> to get started! Do that first and then drop any other questions you have in <#627907039808585758>",
     ],
   },
 ];

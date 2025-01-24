58daf52184c009cab6bde4a7f5d46c1978320dd0
parthkoshti <30394245+parthkoshti@users.noreply.github.com>
2025-01-24 22:22:27 +0530

    removed probability from reacts fn

--- src/autoReacts.ts
+++ src/autoReacts.ts
@@ -1,17 +1,10 @@
 import { Events } from "discord.js";
 import { discordClient } from "./initializeBot";
 import { reactMentions } from "./reactMentions";
-import { probability } from "./probability";
 
 export function autoReacts() {
   discordClient.on(Events.MessageCreate, async (message) => {
     if (message.author.bot) return;
-
-    const react = probability(0.5);
-    if (!react) {
-      console.log("No meme for you!");
-      return;
-    }
 
     const matchedReact = reactMentions.find((pair) =>
       pair.listenFor.some((keyword) =>

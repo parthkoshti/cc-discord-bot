224097e4b4796c35097b7e66bb7fcbde84a4d173
parthkoshti <30394245+parthkoshti@users.noreply.github.com>
2025-01-24 21:46:50 +0530

    added how to use the CC to noobPairs

--- src/noobPairs.ts
+++ src/noobPairs.ts
@@ -2,7 +2,7 @@ export const noobPairs = [
   {
     listenFor: ["new"],
     responses: [
-      "Welcome to the server, newbie. Go to <#922612850457997323> and <#922573595115421796> to get started! Do that first and then drop any other questions you have in <#627907039808585758>",
+      "Welcome to the server, newbie. Go to <#922612850457997323>, <#922612850457997323> and <#922573595115421796> to get started! Do that first and then drop any other questions you have in <#627907039808585758>",
     ],
   },
 ];
